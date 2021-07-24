b598fb74dad7b1f3ba1c9f57e66ad91ac78180d6
afifialaa <alaaahmedafifi@gmail.com>
2021-07-24 08:13:51 +0200

    category CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,54 @@
 class CategoriesController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @categories = Category.all
+        render(json: {categories: @categories}, status: :ok)
+    end
+
+    def show
+        @category = Category.find_by_id(params[:id])
+        if !@category
+            render(json: {message: "Category was not found"}, status: :not_found)
+        else
+            render(json: {category: @category}, status: :ok)
+        end
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            render(json: {message: "Category was created successfully"}, status: :created)
+        else
+            render(json: {message: "Category was not created"}, status: :unprocessable_entity)
+        end
+    end
+
+    def update
+        @category = Category.find_by_id(params[:id])
+        if !@category
+            render(json: {message: "Category was not found"}, status: :not_found)
+        elsif @category.update(category_params)
+            render(json: {message: "Category was updated successfully"}, status: :ok)
+        else
+            render(json: {message: "Category was not updated"}, status: :unprocessable_entity)
+        end
+    end
+
+    def destroy
+        @category = Category.find_by_id(params[:id])
+        if !@category
+            render(json:{message: "Category was not found"}, status: :not_found)
+        elsif @category.destroy
+            render(json:{message: "Category was deleted successfully"}, status: :ok)
+        else
+            render(json:{message: "Category was not deleted"}, status: :unprocessable_entity)
+        end
+    end
+
+    private
+    def category_params
+        params.permit(:name, :description)
+    end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,51 +3,51 @@ class ProductsController < ApplicationController
 
   skip_before_action :verify_authenticity_token
 
-  def index
-    @products = Product.all
-    render(json: {products: @products}, status: :ok)
-  end
-
-  def show
-    @product = Product.find_by_id(params[:id])
-    if !@product
-      render(json: {message: "Product was not found"}, status: :not_found)
-    elsif
-      render(json: {product: @product}, status: :ok)
+    def index
+        @products = Product.all
+        render(json: {products: @products}, status: :ok)
     end
-  end
 
-  def create
-    @product = Product.new(product_params)
-    if @product.save
-      render(json: { message: "Product was created successfully"}, status: :created)
-    else
-      render(json: { message: "Product was not created successfully"}, status: :unprocessable_entity)
+    def show
+        @product = Product.find_by_id(params[:id])
+        if !@product
+            render(json: {message: "Product was not found"}, status: :not_found)
+        elsif
+            render(json: {product: @product}, status: :ok)
+        end
     end
-  end
 
-  def destroy
-    @product = Product.find_by_id(params[:id])
-    if !@product
-      render(json: {message: "Product was not found"}, status: :not_found)
-    elsif @product.destroy
-      render(json: {message: "Product was deleted successfully"}, status: :ok)
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            render(json: { message: "Product was created successfully"}, status: :created)
+        else
+            render(json: { message: "Product was not created successfully"}, status: :unprocessable_entity)
+        end
     end
-  end
+
+    def destroy
+        @product = Product.find_by_id(params[:id])
+        if !@product
+            render(json: {message: "Product was not found"}, status: :not_found)
+        elsif @product.destroy
+            render(json: {message: "Product was deleted successfully"}, status: :ok)
+        end
+    end
   
-  def update
-    @product = Product.find_by_id(params[:id])
-    if !@product
-      render(json: {message: "Product was not found"}, status: :not_found)
-    elsif @product.update(product_params)
-      render(json: {message: "Product was updated successfully"}, status: :ok)
-    else
-      render(json: {message: "Product was not deleted"}, status: :ok)
+    def update
+        @product = Product.find_by_id(params[:id])
+        if !@product
+            render(json: {message: "Product was not found"}, status: :not_found)
+        elsif @product.update(product_params)
+            render(json: {message: "Product was updated successfully"}, status: :ok)
+        else
+            render(json: {message: "Product was not deleted"}, status: :ok)
+        end
     end
-  end
 
-  private
-  def product_params
-    params.permit(:name, :description, :quantity, :price, :status)
-  end
+    private
+    def product_params
+        params.permit(:name, :description, :quantity, :price, :status)
+    end
 end
