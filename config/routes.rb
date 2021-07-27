0d13f2a24cf7a6ba1911435b8ac7ed1c41821bfd
afifialaa <alaaahmedafifi@gmail.com>
2021-07-27 11:40:07 +0200

    cors config for local host

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  post '/suppliers', to: 'suppliers#create'
-  delete '/suppliers/:id', to: 'suppliers#destroy'
-  put '/suppliers/:id', to: 'suppliers#update'
-  get '/suppliers/:id', to: 'suppliers#show'
-  get '/suppliers', to: 'suppliers#index'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    
+    post '/suppliers', to: 'suppliers#create'
+    delete '/suppliers/:id', to: 'suppliers#destroy'
+    put '/suppliers/:id', to: 'suppliers#update'
+    get '/suppliers/:id', to: 'suppliers#show'
+    get '/suppliers', to: 'suppliers#index'
 
-  post '/products', to: 'products#create'
-  delete '/products/:id', to: 'products#destroy'
-  put '/products/:id', to: 'products#update'
-  get '/products/:id', to: 'products#show'
-  get '/products', to: 'products#index'
+    post '/products', to: 'products#create'
+    delete '/products/:id', to: 'products#destroy'
+    put '/products/:id', to: 'products#update'
+    get '/products/:id', to: 'products#show'
+    get '/products', to: 'products#index'
 
-  post '/categories', to: 'categories#create'
-  delete '/categories/:id', to: 'categories#destroy'
-  put '/categories/:id', to: 'categories#update'
-  get '/categories/:id', to: 'categories#show'
-  get '/categories', to: 'categories#index'
+    post '/categories', to: 'categories#create'
+    delete '/categories/:id', to: 'categories#destroy'
+    put '/categories/:id', to: 'categories#update'
+    get '/categories/:id', to: 'categories#show'
+    get '/categories', to: 'categories#index'
+
 end
