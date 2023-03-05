955aac5ce1bb6ed0c8f48832073dfbd7ec5e5a3b
Alaa Afifi <alaaafifi@Alaas-MacBook-Pro.local>
2023-03-05 19:22:25 +0200

    user bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,21 @@ class ApplicationController < ActionController::Base
     def get_employee
         @employee = Employee.find(params[:id])
         if @employee == nil
-            render(json: {"error":"emplyee not found"}, status: :not_found)
+            render(json: {"message":"emplyee not found"}, status: :not_found)
         end
     end
 
 	def get_user
 		@user = User.find_by(email: params[:email])
 		if @user == nil
-			render(json: {"error": "user not found", status: :not_found})
+			render(json: {"message": "User not found"}, status: :not_found)
 		end
 	end
 
     def get_supplier
 		@supplier = Supplier.find(params:id)
 		if @supplier == nil
-			render(json: {"error": "supplier not found"}, status: :not_found)
+			render(json: {"message": "Supplier not found"}, status: :not_found)
 		end
     end
 
